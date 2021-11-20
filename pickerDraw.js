d551a44bff0c7cd4bb44b0f2f7195b2278471cfb
Cyrun <Cyrun2@gmail.com>
2021-11-20 14:32:20 -0500

    first commit

---
+++ pickerDraw.js
@@ -0,0 +1,75 @@
+/**********************************  Start of the Color Wheel ************************************/
+
+const colorPicker = new iro.ColorPicker(".colorPicker", {
+    width: 280,
+    color: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#fff",
+    margin: 50,
+  });
+
+const values = document.getElementById("values");
+const hexInput = document.getElementById("hexInput");
+
+colorPicker.on(["color:init", "color:change"], function(color){
+  values.innerHTML = ["hex: " + color.hexString];
+  hexInput.value = color.hexString;
+});
+
+hexInput.addEventListener('change', function() {
+  colorPicker.color.hexString = this.value;
+});
+
+function resetWheel() {
+    colorPicker.reset();
+}
+
+/**********************************  Start of the Canvas ****************************************/
+
+const canvas = document.getElementById('paint');
+const ctx = canvas.getContext('2d');
+ 
+const sketch = document.getElementById('sketch');
+const sketch_style = getComputedStyle(sketch);
+canvas.width = 500;
+canvas.height = 250;
+
+const mouse = {x: 0, y: 0};
+ 
+/* Mouse Capturing Work */
+canvas.addEventListener('mousemove', function(e) {
+  mouse.x = e.pageX - this.offsetLeft;
+  mouse.y = e.pageY - this.offsetTop;
+}, false);
+
+/* Drawing on Paint App */
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+ctx.strokeStyle = hexInput.value;
+function getColor(colour){ctx.strokeStyle = colour;}
+
+function getSize(size){ctx.lineWidth = size;}
+ 
+canvas.addEventListener('mousedown', function(e) {
+    ctx.beginPath();
+    getColor(hexInput.value);
+    ctx.moveTo(mouse.x, mouse.y);
+ 
+    canvas.addEventListener('mousemove', onPaint, false);
+}, false);
+ 
+canvas.addEventListener('mouseup', function() {
+    canvas.removeEventListener('mousemove', onPaint, false);
+}, false);
+ 
+const onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke();
+};
+
+function resetCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+
